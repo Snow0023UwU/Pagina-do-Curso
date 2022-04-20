5372e471f0ac0f3348b00c0b8cf5e00cb5f50a75
Gui odeia front-end <89114873+Snow0023UwU@users.noreply.github.com>
2022-04-20 08:47:33 -0300

    Add files via upload

---
+++ Banco de dados/src/br/senai/com/Main.java
@@ -0,0 +1,11 @@
+package br.senai.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ Banco de dados/src/br/senai/com/model/Banco.java
@@ -0,0 +1,41 @@
+package br.senai.com.model;
+
+public class Banco {
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereço;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+}
